f0a7e31d48a7908a26b9c046dc2588ae098746e6
Nick003-CSE <99338837+Nick003-CSE@users.noreply.github.com>
2022-03-07 22:27:53 +0530

    Update W2_01.c
    Problem Resolved

--- W2_01.c
+++ W2_01.c
@@ -2,27 +2,19 @@
 #include <string.h>
 int main()  
 {  
-    char first_string[20];  
-    char second_string[20];  
-    int i,l;
-    printf("Enter the first string");  
+    char first_string[20],second_string[20];  
+    int l,i,j=0;
+    printf("Enter the first string ");  
     gets(first_string);  
-    printf("\nEnter the second string");  
+    printf("\nEnter the second string ");  
     gets(second_string);  
-    for(i=0;first_string[i]!='\0';i++)
-    {  
-    for(int j=0;second_string[j]!='\0';j++)  
-    {  
-        
-        first_string[i]=second_string[j];  
-        i++;  
-    } 
-    first_string[i]='\0';
-    }
+   strcat(first_string,second_string);
+   printf("%s\n",first_string);
    l=strlen(first_string);
-   for (int m=l;m>=0;m--)
+   for(i=l-1;i>=0;i--)
    {
-     printf("%s",first_string[m]);
+  printf("%c",first_string[i]);
    }
-return 0;  
+  
+return 0;
 }
